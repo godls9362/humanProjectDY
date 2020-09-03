9df0972ea46b06d259919924b2757b36b3ebbac9
admin <admin@KIMWS>
2020-09-03 16:33:31 +0900

    0903 finished <DTO>

--- src/DTO/Basket_DTO.java
+++ src/DTO/Basket_DTO.java
@@ -1,5 +1,27 @@
 package DTO;
 
 public class Basket_DTO {
-
+	private int no; 
+	private	String userid;
+	private int cnt;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
 }
--- src/DTO/LocalFood_DTO.java
+++ src/DTO/LocalFood_DTO.java
@@ -1,5 +1,35 @@
 package DTO;
 
 public class LocalFood_DTO {
-
+	private int no;
+	private String name;
+	private	int price;
+	private String madeBy;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getMadeBy() {
+		return madeBy;
+	}
+	public void setMadeBy(String madeBy) {
+		this.madeBy = madeBy;
+	}
+	
+	
+	
 }
